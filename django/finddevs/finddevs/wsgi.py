beecea763723ce1c1dc6bab4500c115c720f9667
MakaRonish <ronishmakaju12345@gmail.com>
2024-10-30 01:18:44 -0400

    setup

---
+++ django/finddevs/finddevs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for finddevs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finddevs.settings')
+
+application = get_wsgi_application()
