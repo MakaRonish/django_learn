f1b209065eeb7ac54e520311989587af1e8d431b
MakaRonish <ronishmakaju12345@gmail.com>
2024-11-04 00:08:35 -0500

    models basic

--- django/finddevs/projects/models.py
+++ django/finddevs/projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+# if we use models.Model it tells django it is a model and not just a class and project is the name of table we want
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, blank=True, null=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return self.title
