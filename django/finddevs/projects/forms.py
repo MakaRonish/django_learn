daeb428bfeb4fe5b82d40b77d2095df76b585aa5
MakaRonish <ronishmakaju12345@gmail.com>
2024-11-06 22:49:07 -0500

    CRUD

---
+++ django/finddevs/projects/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Project
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project  # the class we need form for
+        # fields = "__all__"  # all means it will add all the feilds for form except auto generating or non editable ones
+        fields = ["title", "description", "demo_link", "source_link", "tags"]
--- django/finddevs/projects/views.py
+++ django/finddevs/projects/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project, Review, Tag
+from .forms import ProjectForm
 
-projectsList = [
-    {
-        "id": "1",
-        "title": "Ecommerce Website",
-        "description": "Fully functional ecommerce website",
-    },
-    {
-        "id": "2",
-        "title": "Portfolio Website",
-        "description": "A personal website to write articles and display work",
-    },
-    {
-        "id": "3",
-        "title": "Social Network",
-        "description": "An open source project built by the community",
-    },
-]
+# projectsList = [
+#     {
+#         "id": "1",
+#         "title": "Ecommerce Website",
+#         "description": "Fully functional ecommerce website",
+#     },
+#     {
+#         "id": "2",
+#         "title": "Portfolio Website",
+#         "description": "A personal website to write articles and display work",
+#     },
+#     {
+#         "id": "3",
+#         "title": "Social Network",
+#         "description": "An open source project built by the community",
+#     },
+# ]
 
 
 def projects(request):
@@ -54,3 +55,38 @@ def project(request, pk):
 
 
 # Create your views here.
+
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == "POST":
+        # print(request.POST)
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == "POST":
+        # print(request.POST)
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    context = {"object": project}
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects")
+
+    return render(request, "projects/delete_template.html", context)
