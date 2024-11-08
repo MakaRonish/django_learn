847b798aa855f43b034fe10b90d89f758ee7ba09
MakaRonish <ronishmakaju12345@gmail.com>
2024-11-08 00:07:28 -0500

    static files

--- django/finddevs/projects/forms.py
+++ django/finddevs/projects/forms.py
@@ -6,4 +6,11 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project  # the class we need form for
         # fields = "__all__"  # all means it will add all the feilds for form except auto generating or non editable ones
-        fields = ["title", "description", "demo_link", "source_link", "tags"]
+        fields = [
+            "title",
+            "featured_image",
+            "description",
+            "demo_link",
+            "source_link",
+            "tags",
+        ]
