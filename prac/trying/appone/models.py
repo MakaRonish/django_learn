5a497be38ae4b4df01c5e443b210d84a3f074721
MakaRonish <ronishmakaju12345@gmail.com>
2024-11-07 22:38:28 -0500

    prac

--- prac/trying/appone/models.py
+++ prac/trying/appone/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
+
+
+class database(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=2000, blank=True, null=True)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
 
 # Create your models here.
--- prac/trying/appone/views.py
+++ prac/trying/appone/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import database
 
 
 def page1(request):
-    return render(request, "appone/page1.html")
+    items = database.objects.all()
+    context = {"item": items}
+    return render(request, "appone/page1.html", context)
 
 
 def page2(request):
