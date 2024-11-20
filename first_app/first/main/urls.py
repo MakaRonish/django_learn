f429e4746d8b46e93fa2e45f55c2059eb44efe17
MakaRonish <ronishmakaju12345@gmail.com>
2024-11-19 21:55:15 -0500

    edit done

--- first_app/first/main/urls.py
+++ first_app/first/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.home, name="root"),
     path("block2", views.page2, name="page2"),
     path("addbook", views.add_book, name="AddBook"),
+    path("editbook/<str:pk>/", views.edit, name="edit"),
 ]
--- first_app/first/main/views.py
+++ first_app/first/main/views.py
@@ -25,4 +25,17 @@ def add_book(request):
     return render(request, "main/bookForm.html", context)
 
 
+def edit(request, pk):
+    books = models.Books_table.objects.get(id=pk)
+    book_form = Bookform(instance=books)
+    context = {"book_form": book_form}
+    if request.method == "POST":
+        form = Bookform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("root")
+
+    return render(request, "main/bookForm.html", context)
+
+
 # Create your views here.
