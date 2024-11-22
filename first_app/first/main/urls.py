9a17e03235c6cf6d58619863073bb9e98295ab6f
MakaRonish <ronishmakaju12345@gmail.com>
2024-11-21 20:32:45 -0500

    single pages

--- first_app/first/main/urls.py
+++ first_app/first/main/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="root"),
-    path("block2", views.page2, name="page2"),
+    path("block2/<str:pk>/", views.page2, name="page2"),
     path("addbook", views.add_book, name="AddBook"),
     path("editbook/<str:pk>/", views.edit, name="edit"),
+    path("delete/<str:pk>/", views.delete, name="delete"),
 ]
--- first_app/first/main/views.py
+++ first_app/first/main/views.py
@@ -9,8 +9,14 @@ def home(request):
     return render(request, "main/block1.html", context)
 
 
-def page2(request):
-    return render(request, "main/block2.html")
+def page2(request, pk):
+    book = models.Books_table.objects.get(id=pk)
+    publisher = models.Books_table.objects.all()
+    context = {
+        "book": book,
+        "publisher": publisher,
+    }
+    return render(request, "main/block2.html", context)
 
 
 def add_book(request):
@@ -30,12 +36,18 @@ def edit(request, pk):
     book_form = Bookform(instance=books)
     context = {"book_form": book_form}
     if request.method == "POST":
-        form = Bookform(request.POST)
+        form = Bookform(request.POST, instance=books)
         if form.is_valid():
             form.save()
             return redirect("root")
 
     return render(request, "main/bookForm.html", context)
+
+
+def delete(request, pk):
+    books = models.Books_table.objects.get(id=pk)
+    books.delete()
+    return redirect("root")
 
 
 # Create your views here.
