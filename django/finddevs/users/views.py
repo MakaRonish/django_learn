d25042e6c6462fa311120fb2ce637b319bef4e3d
MakaRonish <ronishmakaju12345@gmail.com>
2024-11-24 00:46:40 -0500

    Profiles

--- django/finddevs/users/views.py
+++ django/finddevs/users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Profile, Skill
 
 
 def profiles(request):
-    return render(request, "users/profiles.html")
+    profiles = Profile.objects.all()
+    Skills = Skill.objects.all()
+    context = {"profiles": profiles, "skills": Skills}
+    return render(request, "users/profiles.html", context)
 
 
 # Create your views here.
